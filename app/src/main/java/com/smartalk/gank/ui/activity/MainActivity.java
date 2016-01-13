0fa27916d99d03868c840d954105fb4ab46cb02b
潘磊 <panlei106@gmail.com>
2016-01-13 21:55:48 +0800

    修复bug

--- app/src/main/java/com/smartalk/gank/ui/activity/MainActivity.java
+++ app/src/main/java/com/smartalk/gank/ui/activity/MainActivity.java
@@ -69,23 +69,15 @@ public class MainActivity extends ToolBarActivity<MainPresenter> implements
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        if (item.getItemId() == R.id.action_settings) {
+            System.out.print("hello");
         }
-
         return super.onOptionsItemSelected(item);
     }
 
